fd4f915969f1d747f18733c85f40a4f66df8b6e3
Brett Tanner <bretttanner171@gmail.com>
2024-05-28 22:35:00 +0900

    Get list of CSVs in target dir

---
+++ csv_transaction_parser.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"strings"
+)
+
+func main() {
+	inputCSVs := getCSVs("/Users/brett/Downloads/new_transactions")
+	fmt.Println(inputCSVs)
+	// ParseTransactions(inputCSVs)
+}
+
+func getCSVs(path string) []fs.DirEntry {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var inputCSVs []fs.DirEntry
+	for _, file := range files {
+		filenameSegments := strings.Split(file.Name(), ".")
+		if filenameSegments[len(filenameSegments)-1] != "csv" {
+			continue
+		}
+
+		inputCSVs = append(inputCSVs, file)
+	}
+
+	return inputCSVs
+}
+
+// func ParseTransactions(inputFiles []fs.DirEntry) {
+// 	for _, file := range inputFiles {
+//
+// 		inputFile, err := os.Open(file.Name())
+// 		defer inputFile.Close()
+// 		outputFile, err := os.Create(file.Name() + "output")
+// 		outputWriter := csv.NewWriter(outputFile)
+// 		defer outputFile.Close()
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+//
+// 		rows, err := csv.NewReader(inputFile).ReadAll()
+// 		for i, row := range rows {
+// 			if i == 0 {
+// 				translateHeaders(outputWriter, row)
+// 			}
+//
+// 			translateRow(outputWriter, row)
+// 		}
+// 	}
+// }
+//
+// func translateHeaders(w *csv.Writer, row []string) [4]string {
+// 	return [4]string{"", "", "", ""}
+// }
+//
+// func translateRow(w *csv.Writer, row []string) [4]string {
+// 	for i, cell := range row {
+// 		switch i {
+// 		case 0:
+// 		}
+// 	}
+// 	return [4]string{"", "", "", ""}
+// }
